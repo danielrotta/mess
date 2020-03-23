66e7382536df7a0578a16ea005e058b2e6b172e0
danielrotta <danielrotta13@gmail.com>
2020-03-23 18:32:22 -0500

    fixed dumb error

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 int main() {
     for (int i = 0; i < 5; i++) {
-        system("start iexplore");
-        return 0;
+        system("start iexplore");   
     }
+    return 0;
 }
